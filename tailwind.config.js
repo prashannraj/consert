79721a40996f35fb3e500a38181e6015307790b2
Prashann Raj Karn <111368441+prashannraj@users.noreply.github.com>
2023-12-21 16:03:31 +0545

    vendor and storage reamaing

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    darkMode: 'class',
+    content: ['./resources/**/*.blade.php', './vendor/filament/**/*.blade.php'],
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
